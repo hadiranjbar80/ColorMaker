2eb839125ceb5c07ebada7e202dbde98694b5eba
Mr.Ranjbar <Mr.Ranjbar@DESKTOP-ND2I6SR>
2025-03-22 09:42:48 +0330

    Add project files.

---
+++ ColorMaker/MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using Microsoft.Extensions.Logging;
+
+namespace ColorMaker
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+             .UseMauiApp<App>()
+             // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+             .UseMauiCommunityToolkit()
+             // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+             .ConfigureFonts(fonts =>
+             {
+                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+             });
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
